2f1ee086b11427ce5edbd4dbb98e96e3b33be72c
rilewis967 <fan.of.melissa.etheridge@gmail.com>
2020-05-08 20:33:22 -0700

    Publish up to and including Day 50

--- videos.js
+++ videos.js
@@ -2,7 +2,7 @@ var videos = [
      ["https://www.youtube.com/embed/5t664M1pp_s","Day 1","03/16/2020","00:01",""]
 	,["https://www.youtube.com/embed/yPChpLVCNzg","Day 2","03/17/2020","00:01",""]
     ,["https://www.youtube.com/embed/zD_RJgZDEVQ","Day 3","03/18/2020","00:05",""]
-	,["https://www.youtube.com/embed/b31rgNEvCdE","Day 4","03/19/2020","00:01","Songs that appeared on soundtracks"]
+	,["https://www.youtube.com/embed/b31rgNEvCdE","Day 4","03/19/2020","00:01","Soundtrack Song Day"]
 	,["https://www.youtube.com/embed/1e92AM2q1qg","Day 5","03/20/2020","00:01",""]
 	,["https://www.youtube.com/embed/pGcBXb1Gb1s","Day 6","03/21/2020","00:01","Kenny Rogers R.I.P."]
 	,["https://www.youtube.com/embed/EB0RI8jMsG0","Day 7","03/22/2020","00:01","Piano Day"]
@@ -20,7 +20,7 @@ var videos = [
 	,["https://www.youtube.com/embed/HboLm9va6ug","Day 19","04/03/2020","00:01","Bill Withers R.I.P."]
 	,["https://www.youtube.com/embed/OlJeAJRV-OE","Day 20","04/04/2020","00:01",""]
     ,["https://www.youtube.com/embed/N3Q-LqaU20w","Day 21","04/05/2020","00:01","Piano Day"]
-	,["https://www.youtube.com/embed/AQk2NmvnK1A","Day 22","04/06/2020","00:01","The Making of Scarecrow (starting at 21:30)"] 
+	,["https://www.youtube.com/embed/AQk2NmvnK1A","Day 22","04/06/2020","00:01","Story: The making of Scarecrow (sorry, loud amp at 24:05)"] 
     ,["https://www.youtube.com/embed/TdpyxBIoQ6U","Day 23","04/07/2020","00:01",""]
 	,["https://www.youtube.com/embed/BUe9lkF5aBg","Day 24","04/08/2020","00:01","John Prine R.I.P."]
     ,["https://www.youtube.com/embed/CJ7X6Bb4TpY","Day 25","04/09/2020","00:01","Linda's premiere in front of the camera"]
@@ -28,30 +28,39 @@ var videos = [
 	,["https://www.youtube.com/embed/V4LtQvdyPIA","Day 27","04/11/2020","00:01","Janis Joplin Day<br>So many stories - getting Janis' beads, performing at the tribute, ..."]
 	,["https://www.youtube.com/embed/wMfj609QeT0","Day 28","04/12/2020","00:01","Piano Day<br>- so that's why Bruce laughed"]
 	,["https://www.youtube.com/embed/77jTAfPlx8A","Day 29","04/13/2020","00:01",""]
-	,["https://www.youtube.com/embed/uQJVj5nMRtM","Day 30","04/14/2020","00:01","Sugar pie, honey comb"]
+	,["https://www.youtube.com/embed/uQJVj5nMRtM","Day 30","04/14/2020","00:01","&#34;Sugar pie, honey comb&#34;"]
 	,["https://www.youtube.com/embed/GT3zNrUETts","Day 31","04/15/2020","00:01","&#34;Where's the girl I saw in the bar, playing?&#34;"]
-	,["https://www.youtube.com/embed/xxap6edNBUw","Day 32","04/16/2020","09:33","The story behind Tuesday Morning"]
-	,["https://www.youtube.com/embed/ds7_SnteJEk","Day 33","04/17/2020","00:01","Daughter Duet Day<br>Full intro included in Teach Your Children"]
+	,["https://www.youtube.com/embed/xxap6edNBUw","Day 32","04/16/2020","09:33","Story: The making of Tuesday Morning"]
+	,["https://www.youtube.com/embed/ds7_SnteJEk","Day 33","04/17/2020","00:01","Daughter Duet Day I<br>Full intro included in Teach Your Children"]
 	,["https://www.youtube.com/embed/vCLBx8q4luc","Day 34","04/18/2020","00:01",""]
-	,["https://www.youtube.com/embed/7JxHyNwUHmA","Day 35","04/19/2020","09:21","Piano Day & Daughter Duet II"]
+	,["https://www.youtube.com/embed/7JxHyNwUHmA","Day 35","04/19/2020","09:21","Piano Day<br>Daughter Duet Day II"]
 	,["https://www.youtube.com/embed/t76JEEFCy1c","Day 36","04/20/2020","00:01","4 20"]
 	,["https://www.youtube.com/embed/eifEZv8-7wI","Day 37","04/21/2020","10:00","Tribute to Prince (Kiss) during 4th song (around 43:50)"]
 	,["https://www.youtube.com/embed/N5ZaHbKx8zc","Day 38","04/22/2020","00:01",""]
-    
-    
-	,["https://www.youtube.com/embed/775L7BebPUU","Day 39","04/23/2020","00:01","Story time: The Travels of Monarch X by Ross E. Hutchins"]
+    ,["https://www.youtube.com/embed/775L7BebPUU","Day 39","04/23/2020","00:01","Story time: The Travels of Monarch X by Ross E. Hutchins"]
 	,["https://www.youtube.com/embed/nBdlJtTsJXQ","Day 40","04/24/2020","00:01","Bruce Springsteen Day"]
+// END OF DELIVERED
+    
+// FINISHED PROCESSING - HOLDING LOCALLY
+    ,["https://www.youtube.com/embed/ctODCfZLRO4","Day 41","04/25/2020","10:00","Daughter Duet Day III"]
+    ,["https://www.youtube.com/embed/Jfkf6cwmlp8","Day 42","04/26/2020","00:01","Piano Day<br>Memorial for the Nova Scotia tragedy, and for survivors of the Parkland shooting"]
+	,["https://www.youtube.com/embed/ei_CqVoC9K0","Day 43","04/27/2020","00:01",""]
+	,["https://www.youtube.com/embed/G5LqmXESHzE","Day 44","04/28/2020","00:01","&#34;I don't know. For a background singer, she's pretty loud.&#34;"]
+	,["https://www.youtube.com/embed/JdhDGobUVo0","Day 45","04/29/2020","00:01","Cover Song Day"]
+	,["https://www.youtube.com/embed/1o68sSNjesc","Day 46","04/30/2020","00:01","Coffee with Linda I"]
+	,["https://www.youtube.com/embed/q_aEn0UdBkU","Day 47","05/01/2020","00:01","Make Up Song Day"]
+	,["https://www.youtube.com/embed/yR-QuJ5x_4o","Day 48","05/02/2020","00:01",""]
+    
+// IN PROGRESS - SHOWING LOCALLY
+	,["https://www.youtube.com/embed/YzHUOQDoB8M","Day 49","05/03/2020","00:01","Piano Day<br>The Eagles Day<br>&#34;All the best songs are about girls. You can quote me.&#34;"]
+	,["https://www.youtube.com/embed/ADmqkAf6qvQ","Day 50","05/04/2020","00:01","Day 50: The Half-century"]
+    
+// AWAITING PROCESSING
+//	,["https://www.youtube.com/embed/","Day 51","05/05/2020","00:01","Day 51"]
 
-    
-//	,["https://www.youtube.com/embed/ctODCfZLRO4","Day 41","04/25/2020","10:00","Day 41"]
-//    ,["https://www.youtube.com/embed/Jfkf6cwmlp8","Day 42","04/26/2020","00:01","Memorial for the Nova Scotia tragedy<br>Also, for survivors of the Parkland shotting"]
-//	,["https://www.youtube.com/embed/ei_CqVoC9K0","Day 43","04/27/2020","00:01","Day 43"]
-//	,["https://www.youtube.com/embed/G5LqmXESHzE","Day 44","04/28/2020","00:01","Day 44"]
-//	,["https://www.youtube.com/embed/JdhDGobUVo0","Day 45","04/29/2020","00:01","Day 45"]
-//	,["https://www.youtube.com/embed/1o68sSNjesc","Day 46","04/30/2020","00:01","Day 46"]
-    
-    
-//	,["https://www.youtube.com/embed/","Day 47","05/01/2020","00:01","Day 47"]
-//	,["https://www.youtube.com/embed/","Day 48","05/02/2020","00:01","Day 48"]
-//	,["https://www.youtube.com/embed/","Day 49","05/03/2020","00:01","Day 49"]
+
+// NOT AVAILABLE IN YOUTUBE YET
+//	,["https://www.youtube.com/embed/","Day 52","05/06/2020","00:01","Day 52"]
+//	,["https://www.youtube.com/embed/","Day 53","05/07/2020","00:01","Day 53"]
+//	,["https://www.youtube.com/embed/","Day 54","05/08/2020","00:01","Buscuit Day"]
 ];
